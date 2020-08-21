e0730944fbd0be2f8cf4ab53ed4f8b1d3b2d375f
Kyle Patterson <patterkyle@gmail.com>
2020-08-21 12:42:23 -0500

    can draw a golf hand in a container

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import './style.css';
 import * as PIXI from 'pixi.js';
 
+const log = console.log;
+
+const appState = {
+};
+
 const appWidth = 256;
 const appHeight = 256;
+
+const cardWidth = 240;
+const cardHeight = 336;
+const cardScaleX = 0.3;
+const cardScaleY = 0.3;
 
 const app = new PIXI.Application({
   width: appWidth,
   height: appHeight,
   antialias: true,
+  resolution: 1
 });
 
 app.renderer.backgroundColor = 0xeeeeee;
@@ -35,16 +46,78 @@ const cardFiles = [
   "cards/9S.svg", "cards/JD.svg", "cards/KS.svg", "cards/TD.svg",
 ];
 
-function getCardSprite(cardName) {
+let sprites = {};
+
+function loadCardSprite(filename) {
+  // TODO replace w/ regex
+  const cardName = filename.split('/')[1].split('.')[0];
   const sprite = new PIXI.Sprite(app.loader
-				 .resources[`cards/${cardName}.svg`]
-				 .texture);
-  return sprite;
+  				 .resources[filename]
+  				 .texture);
+  sprite.cardName = cardName;
+  sprite.interactive = true;
+  sprite.on('click', onCardClick);
+  sprites[cardName] = sprite;
 }
 
 function setup() {
-  const card2B = getCardSprite("2B");
-  app.stage.addChild(card2B);
+  cardFiles.map(filename => loadCardSprite(filename));
+
+  drawHand(['3S', 'JH', '5H', 'KC', '8D', 'AS'], 'top');
+  // drawHand(['3S', 'JH', '5H', 'KC', '8D', 'AS'], 'left');
+
+  app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta) {
+}
+
+function getCardSprite(cardName, x, y) {
+  const sprite = sprites[cardName];
+
+  sprite.x = x;
+  sprite.y = y;
+  sprite.scale.set(cardScaleX, cardScaleY);
+
+  return sprite;
+}
+
+function drawHand(cards, pos) {
+  const container = new PIXI.Container();
+  const xSpacing = 5;
+  const ySpacing = 5;
+
+  let sprites = [];
+  for (let i = 0; i < 3; i++) {
+    sprites.push(getCardSprite(cards[i],
+			       i * (cardWidth * cardScaleX) + (xSpacing * i),
+			       0));
+  }
+
+  for (let i = 3; i < 6; i++) {
+    const j = i - 3;
+    sprites.push(getCardSprite(cards[i],
+			       j * (cardWidth * cardScaleX) + (xSpacing * j),
+			       cardHeight * cardScaleY + ySpacing));
+  }
+
+  sprites.map(sprite => container.addChild(sprite));
+
+  switch (pos) {
+  case 'top':
+    container.x = app.screen.width / 2 - container.width / 2;
+    break;
+
+  case 'left':
+    // container.angle = 90;
+    break;
+  }
+
+  app.stage.addChild(container);
+}
+
+function onCardClick(event) {
+  console.log(this.cardName);
 }
 
 app.loader.add(cardFiles).load(setup);
